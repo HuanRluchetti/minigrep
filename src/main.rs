0f3a3d108b5447bb33039318f5084d58bf23074f
Huan <huan.luchetti@gmai.com>
2024-04-27 00:33:34 +0000

    feat: added new test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::env;
-use std::fs;
 use std::process;
 
 use minigrep::Config;
@@ -20,29 +19,3 @@ fn main() {
         process::exit(1);
     }
 }
-
-// fn run (config: Config) -> Result<(), Box<dyn Error>> {
-//     let content = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-//     println!("With text:\n{contents}");
-
-//     Ok(())
-// }
-
-// struct Config {
-//     query: String,
-//     file_path: String,
-// }
-
-// impl Config {
-//     fn build(args: &[String]) -> Result<Config, &'static str> {
-//         if args.len() < 3 {
-//             return Err("not enough arguments");
-//         }
-
-//         let query = args[1].clone();
-//         let file_path = args[2].clone();
-
-//         Ok(Config { query, file_path })
-//     }
-// }
